41b8ede5da55a40df3ae8f63289ed7a2192d6691
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-01-01 23:04:44 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{    
+    printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
+    return 0;
+}
