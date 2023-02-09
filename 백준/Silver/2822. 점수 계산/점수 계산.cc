e7e929fffb4af78b309bcf1587c950b944bb8b8a
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-02-09 20:20:40 +0900

    [Silver V] Title: 점수 계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool Sort_Compare(pair<int, int> _pair0, pair<int, int> _pair1)
+{
+	return _pair0.second > _pair1.second;
+}
+
+int main()
+{
+	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+	pair<int, int> pr_Idx_Que_Arr[8];
+	int iTotal = 0;
+
+	for (int i = 0; i < 8; ++i)
+	{
+		pr_Idx_Que_Arr[i].first = i + 1; // 몇 번째 문제
+		cin >> pr_Idx_Que_Arr[i].second; // 점수
+	}
+
+	sort(pr_Idx_Que_Arr, pr_Idx_Que_Arr + 8, Sort_Compare);
+
+	int iIndxArr[5] = {};
+	for (int i = 0; i < 5; ++i)
+	{
+		iTotal += pr_Idx_Que_Arr[i].second;
+		iIndxArr[i] = pr_Idx_Que_Arr[i].first;
+	}
+	cout << iTotal << endl;
+	
+	sort(iIndxArr, iIndxArr + 5);
+
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << iIndxArr[i] << " "; // 점수
+	}
+
+
+	return 0;
+}
