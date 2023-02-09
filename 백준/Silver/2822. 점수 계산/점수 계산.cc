8e1b0bee70b1f4f63143edc61e6f39bcb7dc1994
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-02-09 20:23:38 +0900

    [Silver V] Title: 점수 계산, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.cc
+++ 백준/Silver/2822. 점수 계산/점수 계산.cc
@@ -14,22 +14,22 @@ int main()
 	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 
-	pair<int, int> pr_Idx_Que_Arr[8];
+	pair<int, int> pr_Idx_Scr_Arr[8];
 	int iTotal = 0;
 
 	for (int i = 0; i < 8; ++i)
 	{
-		pr_Idx_Que_Arr[i].first = i + 1; // 몇 번째 문제
-		cin >> pr_Idx_Que_Arr[i].second; // 점수
+		pr_Idx_Scr_Arr[i].first = i + 1; // 몇 번째 문제
+		cin >> pr_Idx_Scr_Arr[i].second; // 점수
 	}
 
-	sort(pr_Idx_Que_Arr, pr_Idx_Que_Arr + 8, Sort_Compare);
+	sort(pr_Idx_Scr_Arr, pr_Idx_Scr_Arr + 8, Sort_Compare);
 
 	int iIndxArr[5] = {};
 	for (int i = 0; i < 5; ++i)
 	{
-		iTotal += pr_Idx_Que_Arr[i].second;
-		iIndxArr[i] = pr_Idx_Que_Arr[i].first;
+		iTotal += pr_Idx_Scr_Arr[i].second;
+		iIndxArr[i] = pr_Idx_Scr_Arr[i].first;
 	}
 	cout << iTotal << endl;
 	
