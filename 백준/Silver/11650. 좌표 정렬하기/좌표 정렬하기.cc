3faaf9113416ae510aed942d731a5c934d2d0530
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-02-10 10:02:17 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 56 ms, Memory: 2808 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool Sort_Compare(pair<int, int> _a, pair<int, int> _b)
+{
+	if (_a.first != _b.first)
+		return _a.first < _b.first;
+	else
+		return _a.second < _b.second;
+}
+
+int main()
+{
+	ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	vector<pair<int, int>> vecPair;
+	vecPair.reserve(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		pair<int, int> prTmp;
+		cin >> prTmp.first; // x
+		cin >> prTmp.second; // y
+		vecPair.push_back(prTmp);
+	}
+	sort(vecPair.begin(), vecPair.end(), Sort_Compare);
+
+	for (int i = 0; i< N; ++i)
+	{
+		printf("%d %d\n",vecPair[i].first,vecPair[i].second);
+		// cout << vecPair[i].first << " " << vecPair[i].second << endl;
+	}
+	// vector<pair<int, int>>::iterator iter = vecPair.begin();
+
+	// while (iter != vecPair.end())
+	// {
+	// 	cout << iter->first << " " << iter->second << endl;
+	// 	++iter;
+	// }
+
+
+
+
+	return 0;
+}
