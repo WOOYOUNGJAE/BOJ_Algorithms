781f726c2b5e25a75536ad8b1afcb4b579868b28
WOOYOUNGJAE <76108701+WOOYOUNGJAE@users.noreply.github.com>
2023-03-03 19:15:01 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string strInput;
+    cin >> strInput;
+
+
+    string num = "";
+
+    bool bCountingPlus = true;
+    int answer = 0;
+    int iNums = 0;
+    int plus = 1;
+    
+    int i = 0;
+    while (true)
+    {
+        if (strInput[i] == '\0')
+        {
+            plus = bCountingPlus? 1 : -1;
+            iNums += stoi(num) * plus;
+            answer += iNums;
+
+            cout << answer;
+            return 0;
+        }
+
+        if (strInput[i] == '+')
+        {
+            plus = bCountingPlus? 1 : -1;
+            iNums += stoi(num) * plus;
+
+            num = "";
+            ++i;
+            continue;
+        }
+        else if (strInput[i] == '-')
+        {
+            // if (iNums == 0)
+            // {
+            //     plus = bCountingPlus? 1 : -1;
+            //     iNums = stoi(num) * plus;
+            // }
+            plus = bCountingPlus? 1 : -1;
+            iNums += stoi(num) * plus;
+            answer += iNums;
+
+            bCountingPlus = false;
+            num = "";
+            iNums = 0;
+            ++i;
+            continue;
+        }
+
+        // 숫자 읽기
+        num += strInput[i];
+        ++i;
+    }
+    
+    return 0;
+}
